68e18b7c3167fe7ad34988deb816418fefc35bd1
CodeMichelle <michelle.taylor@parchment.com>
2022-09-29 10:18:20 -0500

    First couple of videos.

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Michelle!");
+
+        int myFirstNumber = 28;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
+    }
+}
