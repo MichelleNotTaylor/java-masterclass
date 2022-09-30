6433becd195c4f1654295d521f32bd064b363821
CodeMichelle <michelle.taylor@parchment.com>
2022-09-30 13:28:35 -0500

    Data types

--- ByteShortIntLong/src/com/parchment/Main.java
+++ ByteShortIntLong/src/com/parchment/Main.java
@@ -3,6 +3,31 @@ package com.parchment;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int myValue = 1000;
+	    int myMinIntValue = Integer.MIN_VALUE;
+	    int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted Maximum Value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Minimum Value = " + (myMinIntValue - 1));
+        int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinlongValue = Long.MIN_VALUE;
+        long myMaxlongValue = Long.MAX_VALUE;
+        System.out.println("long Minimum Value = " + myMinlongValue);
+        System.out.println("long Maximum Value = " + myMaxlongValue);
+        long bigLongLiteralValue = 2_147_483_647_234L;
+
     }
 }
