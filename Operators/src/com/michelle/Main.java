5e02e2f7cec177a380a7ca3edb810f78ed26c4b1
CodeMichelle <michelle.taylor@parchment.com>
2022-10-06 12:07:48 -0500

    Shorthand operators

---
+++ Operators/src/com/michelle/Main.java
@@ -0,0 +1,41 @@
+package com.michelle;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int result = 1 + 2;
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+        result = result - 1;
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previousResult = " + previousResult);
+
+        result = result * 10;
+        System.out.println("2 * 10 = " + result);
+
+        result = result / 5;
+        System.out.println("20 / 5 = " + result);
+
+        result = result % 3;
+        System.out.println("4 % 3 = " + result);
+
+        result++;
+        System.out.println("1 + 1 = " + result);
+
+        result--;
+        System.out.println("2 - 1 = " + result);
+
+        result += 2;
+        System.out.println("1 + 2 = " + result);
+
+        result *= 10;
+        System.out.println("3 * 10 = " + result);
+
+        result /= 3;
+        System.out.println("30 / 3 = " + result);
+
+        result -= 2;
+        System.out.println("10 - 2 = " + result);
+    }
+}
