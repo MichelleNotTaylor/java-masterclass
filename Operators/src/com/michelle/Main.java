bb92e47d567ea3167b50e898eb52442aa47e5596
CodeMichelle <michelle.taylor@parchment.com>
2022-10-06 12:35:07 -0500

    Operators challenge. End of first lesson

--- Operators/src/com/michelle/Main.java
+++ Operators/src/com/michelle/Main.java
@@ -37,5 +37,54 @@ public class Main {
 
         result -= 2;
         System.out.println("10 - 2 = " + result);
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if(topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if (topScore > 90 || secondTopScore <= 90) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+
+
+        double firstDoubleValue = 20.00d;
+        double secondDoubleValue = 80.00d;
+        double doubleSum = (firstDoubleValue + secondDoubleValue) * 100;
+        double remainder = doubleSum % 40.00d;
+
+        boolean isZero = remainder == 0? true : false;
+        System.out.println(isZero);
+
+        if (!isZero) {
+            System.out.println("Got some remainder");
+        }
     }
 }
