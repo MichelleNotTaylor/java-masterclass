c747c50aea32fbd17976cb87f8c07e1c6c2d7c84
CodeMichelle <michelle.taylor@parchment.com>
2022-11-03 22:19:51 -0500

    Switch

---
+++ Switch/src/com/michelle/Main.java
@@ -0,0 +1,52 @@
+package com.michelle;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int value = 1;
+        if(value == 1) {
+            System.out.println("Value was 1");
+        } else if(value == 2) {
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Value was not 1 or 2");
+        }
+
+        int switchValue = 1;
+
+        switch(switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.println("Value was not 1 or 2");
+                break;
+        }
+
+        char switchChar = 'A';
+
+        switch(switchChar) {
+            case 'A':
+                System.out.println("Value was A");
+                break;
+            case 'B':
+                System.out.println("Value was B");
+                break;
+            case 'C':
+                System.out.println("Value was C");
+                break;
+            case 'D':
+                System.out.println("Value was D");
+                break;
+            case 'E':
+                System.out.println("Value was E");
+                break;
+            default:
+                System.out.println("Value not found");
+                break;
+        }
+    }
+}
